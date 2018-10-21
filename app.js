6d8a617cf6b22b1e2945f6e821b38e6e8924da96
Egor Kirpichev <egor.kirpichev@telekom.de>
2018-10-21 17:15:01 +0200

    try read static json 3

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-var json = require('./html-5-elements.json');
-console.log('JSON', json);
+fetch('https://eeegor.github.io/html-5-elements/html-5-elements.json')
+  .then(res => res.json())
+  .then(json => {
+    console.log('JSON', json);
+  })
