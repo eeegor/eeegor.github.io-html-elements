5e7480ed1d834c97f3be1000cd34107ccda22da4
Egor Kirpichev <egor.kirpichev@telekom.de>
2018-10-21 18:27:20 +0200

    loop over available list of elements with javascript and show examples

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 fetch('https://eeegor.github.io/html-5-elements/html-5-elements.json')
   .then(res => res.json())
-  .then(json => {
-    console.log('JSON', json);
+  .then(elements => {
+	console.log('elements', elements);
+	var htmlString = '';
+	elements.forEach(element => {
+		htmlString += '<div>'
+		htmlString += '<h2>' + element.name + '</h2>'
+		htmlString += '<div>' + element.example + '</div>'
+		htmlString += '</div>'
+	});
+	document.getElementById('app').innerHTML = htmlString;
   })
